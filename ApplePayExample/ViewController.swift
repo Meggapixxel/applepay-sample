d953097ec2b856cc688c9cbb3bbbf360bd4edde3
Vladyslav Vcherashnii <vladyslav.vcherashnii@stfalcon.com>
2018-11-05 09:32:11 +0200

    Initial Commit

---
+++ ApplePayExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePayExample
+//
+//  Created by Vladyslav Vcherashnii on 11/5/18.
+//  Copyright © 2018 Stfalcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
